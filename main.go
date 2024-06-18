42f7bf1bb26e4a0d0fe1c1c61718f0591ea596ea
racoelhosilva <racoelhosilva@gmail.com>
2024-06-18 09:44:59 +0100

    feat: daily tide information

--- main.go
+++ main.go
@@ -1,11 +1,59 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-
-	"github.com/racoelhosilva/GoOutside/api"
+	"io"
+	"net/http"
+	"os"
 )
 
+type Weather struct {
+	Location struct {
+		City    string `json:"name"`
+		Country string `json:"country"`
+	} `json:"location"`
+	Forecast struct {
+		ForecastDay []struct {
+			Day struct {
+				Tides []struct {
+					Tide []struct {
+						Time string `json:"tide_time"`
+						Type string `json:"tide_type"`
+					} `json:"tide"`
+				} `json:"tides"`
+			} `json:"day"`
+		} `json:"forecastday"`
+	} `json:"forecast"`
+}
+
 func main() {
-	fmt.Println(api.GetCurrent("Oporto"))
+	city := "Oporto"
+
+	resource := fmt.Sprintf("http://api.weatherapi.com/v1/marine.json?key=%s&q=%s&days=1", os.Getenv("WEATHER_API_KEY"), city)
+	response, err := http.Get(resource)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != 200 {
+		panic("Weather API not available")
+	}
+
+	body, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var weather Weather
+	json.Unmarshal([]byte(body), &weather)
+
+	fmt.Println(weather.Location.City, weather.Location.Country)
+	for _, tide := range weather.Forecast.ForecastDay[0].Day.Tides[0].Tide {
+		fmt.Printf("%-10s %s\n", tide.Type, tide.Time)
+	}
 }
